fa3719c4c049a36b283bd46c6e347869cdc98f6e
WesleyKishore-2054 <wesleykishore07@gmail.com>
2025-08-06 15:24:20 +0530

    Update data-source.ts

--- data-source.ts
+++ data-source.ts
@@ -3,11 +3,11 @@ import { DataSource } from 'typeorm';
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',
-  password: 'wesleyMsd07***',
-  database: 'postgres',
+  url: 'postgresql://wesley_kishore:Jn07hrTbvuFGm1aOQJuKluBEF2Zvb8eq@dpg-d29i6q2li9vc73fosa50-a.oregon-postgres.render.com/backend_db_wesley',
+  ssl: {
+    rejectUnauthorized: false,
+  },
+  synchronize: true, 
   entities: ['src/entities/*.entity.{ts,js}'],
   migrations: ['src/migrations/*.{ts,js}'],
 });
